8797003141eac1675ee09977405b634b8cebbac4
Lukasz Klis <lukasz@klis.pl>
2022-12-01 13:31:27 +0100

    feat: add 2nd solution for Day 1

--- src/01/index.ts
+++ src/01/index.ts
@@ -1,6 +1,6 @@
 import rawInput from './input'
 
-const getMaxCalories = (input: string) => {
+const sortByCalories = (input: string) => {
   return input
     .split('\n\n')
     .map(elf =>
@@ -9,7 +9,12 @@ const getMaxCalories = (input: string) => {
         .map(Number)
         .reduce((prev, curr) => prev + curr, 0)
     )
-    .reduce((prevElf, currElf) => Math.max(prevElf, currElf), 0)
+    .sort((a, b) => (a < b ? 1 : -1))
 }
 
-console.log(getMaxCalories(rawInput))
+const getElfWithMostCalories = (input: string) => sortByCalories(input)[0]
+
+const getThreeElvesWithMostCalories = (input: string) => sortByCalories(input).slice(0, 3)
+
+console.log(getElfWithMostCalories(rawInput))
+console.log(getThreeElvesWithMostCalories(rawInput))
