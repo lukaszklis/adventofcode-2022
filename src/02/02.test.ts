33810089a56211583b2634b780bcc9dca8a2fdca
Lukasz Klis <lukasz@klis.pl>
2022-12-02 20:48:00 +0100

    feat: add test expectation

--- src/02/02.test.ts
+++ src/02/02.test.ts
@@ -1,4 +1,9 @@
 import { describe, it, expect } from 'vitest'
+import { rockPaperScissors } from './02'
+import rawInput from './02.input'
 
 describe('02', () => {
+  it('returns proper score', () => {
+    expect(rockPaperScissors(rawInput)).toEqual(12586)
+  })
 })
