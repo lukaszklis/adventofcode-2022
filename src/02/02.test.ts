13c33927e1ff27ac7a672c34598fedf1fd813391
Lukasz Klis <lukasz@klis.pl>
2022-12-02 21:49:55 +0100

    feat: add Day 2 part 2 solution

--- src/02/02.test.ts
+++ src/02/02.test.ts
@@ -1,9 +1,17 @@
 import { describe, it, expect } from 'vitest'
-import { rockPaperScissors } from './02'
+import { rockPaperScissorsPartOne, rockPaperScissorsPartTwo } from './02'
 import rawInput from './02.input'
 
 describe('02', () => {
-  it('returns proper score', () => {
-    expect(rockPaperScissors(rawInput)).toEqual(13924)
+  describe('part 1', () => {
+    it('returns proper score', () => {
+      expect(rockPaperScissorsPartOne(rawInput)).toEqual(13924)
+    })
+  })
+
+  describe('part 2', () => {
+    it('returns proper score', () => {
+      expect(rockPaperScissorsPartTwo(rawInput)).toEqual(13448)
+    })
   })
 })
--- src/02/02.ts
+++ src/02/02.ts
@@ -10,19 +10,42 @@ enum Result {
   Loss = 0,
 }
 
-const resultsMap: Record<string, number> = {
-  'A X': Shape.Rock + Result.Draw,
-  'A Y': Shape.Paper + Result.Win,
-  'A Z': Shape.Scissors + Result.Loss,
-
-  'B X': Shape.Rock + Result.Loss,
-  'B Y': Shape.Paper + Result.Draw,
-  'B Z': Shape.Scissors + Result.Win,
-
-  'C X': Shape.Rock + Result.Win,
-  'C Y': Shape.Paper + Result.Loss,
-  'C Z': Shape.Scissors + Result.Draw,
+const rockPaperScissorsFactory = (input: string, resultsMap: Record<string, number>): number => {
+  return input.split('\n').reduce((score, round) => score + resultsMap[round], 0)
 }
 
-export const rockPaperScissors = (input: string): number =>
-  input.split('\n').reduce((score, round) => score + resultsMap[round], 0)
+export const rockPaperScissorsPartOne = (input: string): number => {
+  const resultsMap: Record<string, number> = {
+    'A X': Shape.Rock + Result.Draw,
+    'A Y': Shape.Paper + Result.Win,
+    'A Z': Shape.Scissors + Result.Loss,
+
+    'B X': Shape.Rock + Result.Loss,
+    'B Y': Shape.Paper + Result.Draw,
+    'B Z': Shape.Scissors + Result.Win,
+
+    'C X': Shape.Rock + Result.Win,
+    'C Y': Shape.Paper + Result.Loss,
+    'C Z': Shape.Scissors + Result.Draw,
+  }
+
+  return rockPaperScissorsFactory(input, resultsMap)
+}
+
+export const rockPaperScissorsPartTwo = (input: string): number => {
+  const resultsMap: Record<string, number> = {
+    'A X': Shape.Scissors + Result.Loss,
+    'A Y': Shape.Rock + Result.Draw,
+    'A Z': Shape.Paper + Result.Win,
+
+    'B X': Shape.Rock + Result.Loss,
+    'B Y': Shape.Paper + Result.Draw,
+    'B Z': Shape.Scissors + Result.Win,
+
+    'C X': Shape.Paper + Result.Loss,
+    'C Y': Shape.Scissors + Result.Draw,
+    'C Z': Shape.Rock + Result.Win,
+  }
+
+  return rockPaperScissorsFactory(input, resultsMap)
+}
