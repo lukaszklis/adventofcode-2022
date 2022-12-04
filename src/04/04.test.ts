9317cdbe8bf03b6cb5b4d96e4de1b73fdc582181
Lukasz Klis <lukasz@klis.pl>
2022-12-04 09:51:43 +0100

    chore: add day 4 boilerplate

---
+++ src/04/04.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+import { campCleanupPartOne } from './04'
+import rawInput, { exampleInput } from './04.input'
+
+describe('04', () => {
+  describe('part 1', () => {})
+})
