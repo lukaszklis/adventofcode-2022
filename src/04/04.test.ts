70d2d2fba8110fc39fb809eca9819dfac88cf06a
Lukasz Klis <lukasz@klis.pl>
2022-12-05 10:02:57 +0100

    feat: add part 1 for Day 4

--- src/04/04.test.ts
+++ src/04/04.test.ts
@@ -3,5 +3,13 @@ import { campCleanupPartOne } from './04'
 import rawInput, { exampleInput } from './04.input'
 
 describe('04', () => {
-  describe('part 1', () => {})
+  describe('part 1', () => {
+    it('returns correct number of pairs with overlapping assignments for example input', () => {
+      expect(campCleanupPartOne(exampleInput)).toEqual(2)
+    })
+
+    it('returns correct number of pairs with overlapping assignments for raw input', () => {
+      expect(campCleanupPartOne(rawInput)).toEqual(584)
+    })
+  })
 })
--- src/04/04.ts
+++ src/04/04.ts
@@ -1,1 +1,33 @@
-export const campCleanupPartOne = () => {}
+const range = (from: number, to: number): number[] =>
+  [...Array(to - from + 1).keys()].map(i => i + from)
+
+const mapToSection = (elf: string): number[] => {
+  const matches = elf.match(/\d+/g)
+
+  if (!matches) {
+    throw new Error('Invalid section format')
+  }
+
+  const sectionIds = matches.map(Number)
+
+  return range(sectionIds[0], sectionIds[1])
+}
+
+const isSectionOverlapping = (section: number[], otherSection: number[]): boolean =>
+  section.every(id => otherSection.includes(id))
+
+export const campCleanupPartOne = (input: string): number => {
+  const pairs = input.split('\n')
+
+  return pairs.reduce((sum, pair) => {
+    const [elfOne, elfTwo] = pair.split(',')
+    const elfOneSection = mapToSection(elfOne)
+    const elfTwoSection = mapToSection(elfTwo)
+
+    if (elfOneSection.length < elfTwoSection.length) {
+      return sum + Number(isSectionOverlapping(elfOneSection, elfTwoSection))
+    }
+
+    return sum + Number(isSectionOverlapping(elfTwoSection, elfOneSection))
+  }, 0)
+}
