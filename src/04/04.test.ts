f40fd2c32b1cb1efb738049a1ecbcec27d358f8b
Lukasz Klis <lukasz@klis.pl>
2022-12-05 10:04:54 +0100

    fix: test case names

--- src/04/04.test.ts
+++ src/04/04.test.ts
@@ -4,11 +4,11 @@ import rawInput, { exampleInput } from './04.input'
 
 describe('04', () => {
   describe('part 1', () => {
-    it('returns correct number of pairs with overlapping assignments for example input', () => {
+    it('returns correct number of pairs with fully overlapping assignments for example input', () => {
       expect(campCleanupPartOne(exampleInput)).toEqual(2)
     })
 
-    it('returns correct number of pairs with overlapping assignments for raw input', () => {
+    it('returns correct number of pairs with fully overlapping assignments for raw input', () => {
       expect(campCleanupPartOne(rawInput)).toEqual(584)
     })
   })
